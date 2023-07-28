064238bb70d773c75e17808d3229019e29737197
Alexander Shipulin <sania8600@gmail.com>
2023-07-28 19:17:28 +0700

    feat(vite.config): добавлены опции сборки
    Скорректировал путь папки для сборки и очищение папки сборки

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
   assetsInclude: "**/*.hbs",
   plugins: [handlebars({
     partialDirectory: resolve(__dirname, 'src/partials'),
