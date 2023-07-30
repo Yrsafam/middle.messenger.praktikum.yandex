967709260782a5806d2911ca7cb14654caa09dd9
Alexander Shipulin <sania8600@gmail.com>
2023-07-30 18:07:32 +0700

    fix(vite.config): добавлены все роуты как точки входа для vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,16 @@ export default defineConfig({
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        notFound: resolve(__dirname, 'src/404.html'),
+        errorServer: resolve(__dirname, 'src/500.html'),
+        authorization: resolve(__dirname, 'src/authorization.html'),
+        registration: resolve(__dirname, 'src/registration.html'),
+        profile: resolve(__dirname, 'src/profile.html'),
+      }
+    }
   },
   assetsInclude: "**/*.hbs",
   plugins: [handlebars({
