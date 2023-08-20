9449c2d8b4ec025706e8fbffb745027c839377aa
Alexander Shipulin <sania8600@gmail.com>
2023-08-20 16:17:34 +0700

    fix(vite.config): исправлены ошибки линтера
    Исправлены ошибки eslint + форматирование файла

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,38 @@
-import {resolve} from 'path';
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 
-import {chats, messages} from "./public/mock.js";
+import { chats, messages } from "./public/mock";
 
 export default defineConfig({
-  root: resolve(__dirname, 'src'),
-  publicDir: resolve(__dirname, 'public'),
+  root: resolve(__dirname, "src"),
+  publicDir: resolve(__dirname, "public"),
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'src/index.html'),
-        notFound: resolve(__dirname, 'src/404.html'),
-        errorServer: resolve(__dirname, 'src/500.html'),
-        authorization: resolve(__dirname, 'src/authorization.html'),
-        registration: resolve(__dirname, 'src/registration.html'),
-        profile: resolve(__dirname, 'src/profile.html'),
-        profileEdit: resolve(__dirname, 'src/profile-edit.html'),
-        changePassword: resolve(__dirname, 'src/change-password.html'),
-      }
-    }
+        index: resolve(__dirname, "src/index.html"),
+        notFound: resolve(__dirname, "src/404.html"),
+        errorServer: resolve(__dirname, "src/500.html"),
+        authorization: resolve(__dirname, "src/authorization.html"),
+        registration: resolve(__dirname, "src/registration.html"),
+        profile: resolve(__dirname, "src/profile.html"),
+        profileEdit: resolve(__dirname, "src/profile-edit.html"),
+        changePassword: resolve(__dirname, "src/change-password.html"),
+      },
+    },
   },
   assetsInclude: "**/*.hbs",
-  plugins: [handlebars({
-    // Конструкция через resolve не регистрирует эти папки
-    // resolve(__dirname, 'src/partials', 'src/layouts', 'src/pages')
-    partialDirectory: ['src/partials', 'src/layouts', 'src/pages'],
-    context: {
-      chats: chats,
-      messages: messages,
-    }
-  })],
-})
+  plugins: [
+    handlebars({
+      // Конструкция через resolve не регистрирует эти папки
+      // resolve(__dirname, 'src/partials', 'src/layouts', 'src/pages')
+      partialDirectory: ["src/partials", "src/layouts", "src/pages"],
+      context: {
+        chats,
+        messages,
+      },
+    }),
+  ],
+});
