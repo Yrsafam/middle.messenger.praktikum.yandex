7de807f9ec16d2bce69954a5d70e54a4d3d63902
Alexander Shipulin <sania8600@gmail.com>
2023-08-20 17:21:50 +0700

    feat: добавлен registerComponent

---
+++ src/utils/registerComponent.ts
@@ -0,0 +1,44 @@
+import Handlebars, { HelperOptions } from "handlebars";
+import { v4 as makev4uuid } from "uuid";
+
+import { Block, BlockProps } from "./Block.ts";
+
+type ConstructableComponent<Props extends BlockProps = any> = {
+  new (props: Props): Block<Props>;
+  componentName: string;
+};
+
+export function registerComponent(Component: ConstructableComponent) {
+  Handlebars.registerHelper(
+    Component.componentName,
+    // eslint-disable-next-line prefer-arrow-callback
+    function fnDelegate(this: unknown, { hash, data, fn }: HelperOptions) {
+      const component = new Component(hash);
+      const dataAttribute = `data-component-hbs-id-${makev4uuid()}`;
+
+      (data.root__children || data.root.__children || []).push({
+        component,
+        embed(node: DocumentFragment) {
+          const placeholder = node.querySelector(`[${dataAttribute}]`);
+
+          if (!placeholder) {
+            throw new Error(
+              `Can't find data-id for component ${Component.componentName}`,
+            );
+          }
+
+          const { element } = component;
+
+          if (element) {
+            element.append(...Array.from(placeholder.childNodes));
+            placeholder.replaceWith(element);
+          }
+        },
+      });
+
+      const contents = fn ? fn(this) : "";
+
+      return `<div ${dataAttribute}>${contents}</div>`;
+    },
+  );
+}
