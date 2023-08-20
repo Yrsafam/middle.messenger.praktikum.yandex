215cd9b8f0704f367daa38941e5b177faefb9503
Alexander Shipulin <sania8600@gmail.com>
2023-08-20 16:39:29 +0700

    fix(vite.config): удалена обработка шаблонов на этапе сборки

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
-
-import { chats, messages } from "./public/mock";
 
 export default defineConfig({
   root: resolve(__dirname, "src"),
@@ -10,29 +7,5 @@ export default defineConfig({
   build: {
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "src/index.html"),
-        notFound: resolve(__dirname, "src/404.html"),
-        errorServer: resolve(__dirname, "src/500.html"),
-        authorization: resolve(__dirname, "src/authorization.html"),
-        registration: resolve(__dirname, "src/registration.html"),
-        profile: resolve(__dirname, "src/profile.html"),
-        profileEdit: resolve(__dirname, "src/profile-edit.html"),
-        changePassword: resolve(__dirname, "src/change-password.html"),
-      },
-    },
   },
-  assetsInclude: "**/*.hbs",
-  plugins: [
-    handlebars({
-      // Конструкция через resolve не регистрирует эти папки
-      // resolve(__dirname, 'src/partials', 'src/layouts', 'src/pages')
-      partialDirectory: ["src/partials", "src/layouts", "src/pages"],
-      context: {
-        chats,
-        messages,
-      },
-    }),
-  ],
 });
