c4d3ca9759f2072f901f02cce85735c539268004
Alexander Shipulin <sania8600@gmail.com>
2023-08-20 20:27:58 +0700

    fix(renderDom): исправлен рутовый элемент приложения

--- src/utils/renderDom.ts
+++ src/utils/renderDom.ts
@@ -1,7 +1,7 @@
 import { Block } from "./Block.ts";
 
 export function renderDom(block: Block) {
-  const root = document.querySelector(".app");
+  const root = document.querySelector(".root");
   const blockContent = block.getContent();
 
   if (root && blockContent) {
