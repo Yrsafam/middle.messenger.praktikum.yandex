32e4aff317cf7b1dfa5f9e9a9e837bcd686d52d7
Alexander Shipulin <sania8600@gmail.com>
2023-08-21 19:21:29 +0700

    feat: добавлен рендер и регистрация компонентов

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
-// Our code
-console.log('log');
+import { registerComponent } from "./utils/registerComponent.ts";
+import { renderDom } from "./utils/renderDom.ts";
+import { Button } from "./components/Button";
+import { Main } from "./pages/main/Main";
+import { Sidebar } from "./pages/main/Sidebar";
+import { CardChat } from "./pages/main/CardChat";
+
+registerComponent(Button);
+registerComponent(CardChat);
+registerComponent(Sidebar);
+registerComponent(Main);
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderDom(new Main({ someProp: "test" }));
+});
