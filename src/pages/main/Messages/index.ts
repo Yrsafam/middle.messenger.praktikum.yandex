a46dd5771d5da2ec117c48b468748e35ddaa430c
Alexander Shipulin <sania8600@gmail.com>
2023-08-22 23:17:52 +0700

    fix: заменена верстка в шаблонах главной на компоненты

--- src/pages/main/Messages/index.ts
+++ src/pages/main/Messages/index.ts
@@ -1,10 +1,15 @@
 import { Block } from "../../../utils/Block.ts";
+import { messages } from "../../../utils/mocks.ts";
 import template from "./template.hbs";
 
 interface Props {}
 
 export class Messages extends Block<Props> {
   static componentName = "Messages";
+
+  constructor(props: Props) {
+    super({ ...props, messages });
+  }
 
   protected render() {
     return this.compile(template, this.props);
