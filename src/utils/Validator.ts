51d2d933e3f45f7d81d3b7af0ff7b3e3513a5125
Alexander Shipulin <sania8600@gmail.com>
2023-08-25 00:47:11 +0700

    feat: добавлен предварительный класс валидатора

---
+++ src/utils/Validator.ts
@@ -0,0 +1,84 @@
+enum ValidatorRules {
+  FirstName = "first_name",
+  SecondName = "second_name ",
+  Login = "login",
+  Email = "email",
+  Password = "password",
+  Phone = "phone",
+  Message = "message",
+}
+
+type ValidatorValidateResult = {
+  result: boolean;
+  message: string;
+};
+
+export class Validator {
+  private form: HTMLElement;
+
+  constructor(form: HTMLElement) {
+    this.form = form;
+  }
+
+  public checkForm() {
+    const result: Record<string, string> = {};
+    const controls = this.form.querySelectorAll<HTMLInputElement>("[name]");
+
+    controls.forEach((control) => {
+      const errorMessage = this.check(control);
+
+      if (errorMessage.length > 0) {
+        const [first] = errorMessage;
+        result[control.name] = first;
+      }
+    });
+
+    return result;
+  }
+
+  private check(input: HTMLInputElement) {
+    let errors: string[] = [];
+
+    switch (input.name) {
+      case ValidatorRules.Email: {
+        const { result, message } = this.validateEmail(input.value);
+
+        if (!result) {
+          errors.push(message);
+        }
+        break;
+      }
+      default:
+        errors = [];
+        break;
+    }
+
+    return errors;
+  }
+
+  private validate(
+    value: string,
+    regexp: RegExp,
+    message: string,
+  ): ValidatorValidateResult {
+    let result: ValidatorValidateResult = {
+      result: true,
+      message: "",
+    };
+
+    if (!regexp.test(value)) {
+      result = {
+        result: false,
+        message,
+      };
+    }
+
+    return result;
+  }
+
+  private validateEmail(value: string): ValidatorValidateResult {
+    const regexp = /^\S+@\S+\.\S+$/;
+    const ERROR_MESSAGE: string = "Неверный email";
+    return this.validate(value, regexp, ERROR_MESSAGE);
+  }
+}
