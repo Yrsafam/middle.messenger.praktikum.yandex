a08e025fccfc5743c95beca98e5687c0efc84f0b
Alexander Shipulin <sania8600@gmail.com>
2023-08-26 02:28:02 +0700

    feat(Input): проброшены пропсы управления значением и событиями

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,12 +1,16 @@
-import { Block } from "../../utils/Block.ts";
+import { Block, BlockEvents } from "../../utils/Block.ts";
 import template from "./template.hbs";
 
 interface Props {
   name: string;
   label: string;
+  value?: string;
+  onChange?(event: Event): void;
+  onBlur?(event: Event): void;
   type?: string;
   className?: string;
   errorText?: string;
+  events?: BlockEvents;
 }
 
 export class Input extends Block<Props> {
@@ -18,8 +22,22 @@ export class Input extends Block<Props> {
     errorText,
     type = "text",
     className = "",
+    value = "",
+    onChange = () => {},
+    onBlur = () => {},
   }: Props) {
-    super({ name, label, errorText, type, className });
+    super({
+      name,
+      label,
+      errorText,
+      type,
+      className,
+      value,
+      events: {
+        change: onChange,
+        focusout: onBlur,
+      },
+    });
   }
 
   protected render() {
