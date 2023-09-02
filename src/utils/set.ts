a5005c94f16507e9e82af15a5f620097a2768000
Alexander Shipulin <sania8600@gmail.com>
2023-09-02 20:11:52 +0700

    feat: добавлена утилита set

---
+++ src/utils/set.ts
@@ -0,0 +1,20 @@
+import { Indexed, merge } from "./merge.ts";
+
+export function set(
+  object: Indexed | unknown,
+  path: string,
+  value: unknown,
+): Indexed | unknown {
+  if (typeof object !== "object" || object === null) {
+    return object;
+  }
+
+  const result = path.split(".").reduceRight<Indexed>(
+    (acc, key) => ({
+      [key]: acc,
+    }),
+    value as any,
+  );
+
+  return merge(object as Indexed, result);
+}
