3aa3ba5976aa062433f3618879dd8b48d9d8585d
Alexander Shipulin <sania8600@gmail.com>
2023-09-04 20:49:07 +0700

    feat: добавлен предварительный контроллер для авторизации

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,58 @@
+import { authApi, AuthAPI, SigninData, SignupData } from "../api/AuthAPI.ts";
+import { store } from "../utils/Store.ts";
+import router from "../utils/Router.ts";
+import { Routes } from "../utils/renderDom.ts";
+
+export class AuthController {
+  private api: AuthAPI;
+
+  constructor() {
+    this.api = authApi;
+  }
+
+  private async fetchUser() {
+    const response = await this.api.read();
+
+    store.set("user", response);
+  }
+
+  public async checkSignin() {
+    await this.fetchUser();
+  }
+
+  public async signup(data: SignupData) {
+    try {
+      await this.api.signup(data);
+      await this.fetchUser();
+
+      router.go(Routes.Profile);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  public async signin(data: SigninData) {
+    try {
+      await this.api.signin(data);
+      await this.fetchUser();
+
+      router.go(Routes.Profile);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  public async logout() {
+    try {
+      // @TODO добавить закрытие сокет соединений
+
+      await this.api.logout();
+
+      router.go(Routes.Main);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+export const authController = new AuthController();
