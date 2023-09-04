5c274b1d6d7cd6caa2893fcf82b3c9ff0c3bc8c0
Alexander Shipulin <sania8600@gmail.com>
2023-09-04 19:16:25 +0700

    feat: добавлен апи слой для авторизации пользователя

---
+++ src/api/AuthAPI.ts
@@ -0,0 +1,60 @@
+import { BaseAPI } from "./BaseAPI.ts";
+
+export type SignupData = {
+  first_name: string;
+  second_name: string;
+  login: string;
+  email: string;
+  password: string;
+  phone: string;
+};
+
+export type SignupResponse = {
+  id: number;
+};
+
+export type SigninData = {
+  login: string;
+  password: string;
+};
+
+export type AuthUser = {
+  id: number;
+  first_name: string;
+  second_name: string;
+  login: string;
+  email: string;
+  password: string;
+  phone: string;
+  avatar: string;
+};
+
+export class AuthAPI extends BaseAPI {
+  constructor() {
+    super("/auth");
+  }
+
+  public signup(data: SignupData): Promise<SignupResponse> {
+    return this.httpClient.post("/signup", { data });
+  }
+
+  public signin(data: SigninData): Promise<unknown> {
+    return this.httpClient.post("/signin", { data });
+  }
+
+  public logout(): Promise<unknown> {
+    return this.httpClient.post("/logout");
+  }
+
+  public checkSignin(): Promise<AuthUser> {
+    return this.httpClient.get("/user");
+  }
+
+  create = undefined;
+
+  update = undefined;
+
+  delete = undefined;
+
+  read = undefined;
+}
