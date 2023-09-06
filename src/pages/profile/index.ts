62a1da15f14f08c45f3c916e1e4730579ece845e
Alexander Shipulin <sania8600@gmail.com>
2023-09-06 21:25:13 +0700

    feat(Profile): добавлен метод логаута

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -1,7 +1,18 @@
 import { Block } from "../../utils/Block.ts";
 import template from "./template.hbs";
+import { authController } from "../../controllers/AuthController.ts";
 
 export class Profile extends Block {
+  constructor() {
+    super({
+      onLogout: () => this.onLogout(),
+    });
+  }
+
+  public onLogout() {
+    authController.logout();
+  }
+
   protected render() {
     return this.compile(template, this.props);
   }
