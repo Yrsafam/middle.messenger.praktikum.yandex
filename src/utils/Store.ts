a57172041df7f04864fe1bb462e644f4d0573d16
Alexander Shipulin <sania8600@gmail.com>
2023-09-07 21:19:34 +0700

    feat: добавлен начальное состояние для user'а

--- src/utils/Store.ts
+++ src/utils/Store.ts
@@ -1,22 +1,39 @@
 import { set } from "./set.ts";
 import { EventBus } from "./EventBus.ts";
 import { Block, BlockProps } from "./Block.ts";
+import { AuthUser } from "../api/AuthAPI.ts";
 
 export enum StoreEvents {
   Updated = "updated",
 }
 
-export interface StoreState {}
+export interface StoreState {
+  user: AuthUser;
+}
 
-const INITIAL_STATE: StoreState = {};
+const INITIAL_STATE: StoreState = {
+  user: {
+    avatar: "",
+    phone: "",
+    email: "",
+    login: "",
+    id: -1,
+    second_name: "",
+    first_name: "",
+  },
+};
 
 export class Store extends EventBus {
   state: StoreState = INITIAL_STATE;
 
   public set(path: string, newState: unknown) {
-    set(this.state, path, newState);
+    try {
+      set(this.state, path, newState);
 
-    this.emit(StoreEvents.Updated, this.getState);
+      this.emit(StoreEvents.Updated, this.getState);
+    } catch (e) {
+      /* На случай если подписки на событие не произошло */
+    }
   }
 
   public getState() {
