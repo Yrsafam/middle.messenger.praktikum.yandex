258964d44a247ae08329941d11b3804ca4f85964
Alexander Shipulin <sania8600@gmail.com>
2023-09-09 16:48:04 +0700

    feat(UserController): добавлен метод обновления аватара

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,6 +4,7 @@ import { User } from "../shared-kernel/types.ts";
 import { Routes } from "../utils/renderDom.ts";
 import router from "../utils/Router.ts";
 import { store } from "../utils/Store.ts";
+import { getFormattedUser } from "../utils/services.ts";
 
 export class UserController {
   private api: UserAPI;
@@ -17,10 +18,22 @@ export class UserController {
       const response = await this.api.update(data);
 
       router.go(Routes.Settings);
-      store.set("user", response);
+      store.set("user", getFormattedUser(response));
     } catch (e) {
       if (isErrorValidation(e)) {
         alert(`Произошла ошибка обновления профиля: ${e.reason}`);
+      }
+    }
+  }
+
+  async updateAvatar(data: FormData) {
+    try {
+      const response = await this.api.updateAvatar(data);
+
+      store.set("user", getFormattedUser(response));
+    } catch (e) {
+      if (isErrorValidation(e)) {
+        alert(`Произошла ошибка обновления аватара: ${e}`);
       }
     }
   }
