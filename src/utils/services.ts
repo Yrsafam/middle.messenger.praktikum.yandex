d9fe71bab7f5997012d91b44fcea70be47fcb1ea
Alexander Shipulin <sania8600@gmail.com>
2023-09-10 13:41:32 +0700

    feat: добавлен сервис для форматирования сообщений

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,9 +1,20 @@
 import { User } from "../shared-kernel/types.ts";
 import { HTTPTransport } from "./HTTPTransport.ts";
+import { Chat } from "../api/ChatsAPI.ts";
 
 export function getFormattedUser(user: User): User {
   return {
     ...user,
     avatar: HTTPTransport.RESOURCE_URL + user.avatar,
   };
+}
+
+export function getFormattedChats(chats: Chat[], userId: number) {
+  return chats.map((chat) => ({
+    name: chat.title,
+    message: chat.last_message?.content ?? "",
+    lastTime: chat.last_message?.time ?? "",
+    count: chat.unread_count,
+    isMe: chat.last_message?.user.id === userId,
+  }));
 }
