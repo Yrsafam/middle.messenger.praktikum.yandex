dba927e9630b44bfdb748898c36566d61c7a1c34
Alexander Shipulin <sania8600@gmail.com>
2023-09-10 16:39:55 +0700

    feat: добавлено форматирование времени

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -3,6 +3,10 @@ import { HTTPTransport } from "./HTTPTransport.ts";
 import { Chat } from "../api/ChatsAPI.ts";
 import { Message } from "../controllers/MessagesController.ts";
 
+function getTime(date: Date) {
+  return date.toLocaleTimeString().slice(0, 5);
+}
+
 export function getFormattedUser(user: User): User {
   return {
     ...user,
@@ -24,7 +28,9 @@ export function getFormattedChats(chats: Chat[], userId: number): ChatView[] {
     id: chat.id,
     name: chat.title,
     message: chat.last_message?.content ?? "",
-    lastTime: chat.last_message?.time ?? "",
+    lastTime: chat.last_message?.time
+      ? getTime(new Date(chat.last_message?.time))
+      : "",
     count: chat.unread_count,
     isMe: chat.last_message?.user.id === userId,
   }));
@@ -52,6 +58,6 @@ export function getFormattedMessages(
         ? `${HTTPTransport.RESOURCE_URL}${message.file?.path}`
         : "",
     message: message.content,
-    time: message.time,
+    time: getTime(new Date(message.time)),
   }));
 }
