710c9a1f3cafa233d9b2ef9538136d1722b6478c
Alexander Shipulin <sania8600@gmail.com>
2023-09-23 16:02:36 +0700

    feat(HTTPTransport): добавлен тест проверки параметров POST запроса

--- src/utils/HTTPTransport.spec.ts
+++ src/utils/HTTPTransport.spec.ts
@@ -4,6 +4,7 @@ import {
   SinonFakeXMLHttpRequest,
   SinonFakeXMLHttpRequestStatic,
   SinonSpy,
+  spy,
   useFakeXMLHttpRequest,
 } from "sinon";
 import { expect } from "chai";
@@ -137,5 +138,26 @@ describe("HTTPTransport", () => {
 
       expect(request.url).to.equal(`${HTTPTransport.BASE_URL}/auth/signin`);
     });
+  });
+
+  it("should called with correct parameters", () => {
+    const httpPostFake = spy(http, "post");
+
+    httpPostFake("/signin", {
+      data: {
+        a: "a",
+        b: "b",
+      },
+    });
+
+    // eslint-disable-next-line no-unused-expressions
+    expect(
+      httpPostFake.calledWith("/signin", {
+        data: {
+          a: "a",
+          b: "b",
+        },
+      }),
+    ).to.be.true;
   });
 });
